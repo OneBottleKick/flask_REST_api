464d03eb78eaa4ba32228fbde3afe2e883eb5474
OneBottleKick <korean1997@gmail.com>
2020-10-07 21:09:04 +0900

    Fixed bug

--- manage.py
+++ manage.py
@@ -16,10 +16,6 @@ app.register_blueprint(blueprint)
 
 app.app_context().push()
 
-app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
-
-app.app_context().push()
-
 manager = Manager(app)
 
 migrate = Migrate(app, db)
